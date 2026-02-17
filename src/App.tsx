83574e7899efe222b2f774bd74d7bca55bc858d9
chloeleonard397-png <chloeleonard397@gmail.com>
2026-02-17 15:28:38 +0100

    Update App.tsx
    Ajout des noms des participants

--- src/App.tsx
+++ src/App.tsx
@@ -70,8 +70,11 @@ export default function App() {
         </motion.div>
       </AnimatePresence>
 
-      <div className="text-center text-xs text-gray-400 py-6 px-4">
+      <div className="text-center text-xs text-gray-400 py-4">
         Les prix affichés sont fictifs et utilisés uniquement à des fins pédagogiques.
+      </div>
+      <div className="text-center text-xs text-gray-400 py-3">
+         Site créé par CECCALDI Claudia, LEONARD Chloé, PINATEL Arnaud
       </div>
     </div>
   );
